b5e7f415f79b52ace0c79c1790528be125253755
YongERong <yong_e-rong@s2017.sst.edu.sg>
2024-11-23 07:30:23 +0800

    feat: combine grab-tab, gemini-nano demo and tts-stt

--- gemini-nano-demo/index.js
+++ gemini-nano-demo/index.js
@@ -55,11 +55,16 @@
         updateStats();
       }
       const stream = await session.promptStreaming(prompt);
+      var speechPtr = 0;
 
       for await (const chunk of stream) {
         fullResponse = chunk.trim();
         p.innerHTML = fullResponse;
         rawResponse.innerText = fullResponse;
+
+        chrome.tts.speak(fullResponse.slice(speechPtr), {'enqueue': true});
+        // console.log('"' + fullResponse.slice(speechPtr) + '"');
+        speechPtr = fullResponse.length;
       }
     } catch (error) {
       p.textContent = `Error: ${error.message}`;
@@ -200,4 +205,130 @@
     sessionTopK.max = maxTopK;
     await updateSession();
   }
+
+  
+
+  /* TTS STT */
+  var isListening = false;
+  var recognition;
+
+  // Text to Speech
+  document.getElementById("tts-button").addEventListener("click", async () => {
+      let value = document.getElementById("response-area").value;
+      await chrome.tts.speak(value);
+  });
+
+  // Speech to Text
+  document.getElementById("stt-button").addEventListener("click", async () => {
+      isListening = !isListening;
+
+      if (isListening) {
+          // TODO prompt user for microphone access if not given. not sure why cannot.
+          navigator.mediaDevices.getUserMedia({ audio: true })
+              .then((stream) => {
+                  // Microphone permission granted, proceed with recognition
+                  startListening();
+              })
+              .catch((error) => {
+                  alert("Please enable microphone access in your browser settings to use this feature.");
+                  // Permission denied, inform the user
+                  console.error("Microphone access denied:", error);
+              });
+      } else {
+          // End STT
+          // Stop the speech recognition
+          stopListening();
+          console.log("recongtion.stop");
+      }
+  });
+
+  // Clear button
+  document.getElementById("reset-button").addEventListener("click", () => {
+      document.getElementById("prompt-input").value = "";
+  });
+
+
+  function startListening() {
+      document.getElementById("stt-button").innerHTML = "Click to Stop Speaking";
+      document.getElementById("prompt-input").placeholder = "Listening...";
+      document.getElementById("prompt-input").value = "";
+
+      document.getElementById("response-area").innerHTML = "Listening...";
+
+      if (!recognition) {
+      recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+      recognition.continuous = true;
+      recognition.interimResults = true;  // Allows you to get partial results
+      recognition.lang = 'en-US';  // Set the language (optional)
+
+      if (isListening) {
+          stopListening();
+      }
+
+      recognition.onresult = (e) => {
+          let interim = "";
+          let final = "";
+
+          const error = e.results === undefined || (typeof e.results) === "undefined";
+          if (error) {
+              recognition.onend = null;
+              recognition.stop();
+              console.log(error.code)
+              console.error(error);
+              return;
+          }
+
+          for (let i = e.resultIndex; i < e.results.length; ++i) {
+              if (e.results[i].isFinal) {
+                  final += e.results[i][0].transcript;
+              } else {
+                  interim += e.results[i][0].transcript;
+              }
+          }
+
+          if (interim) {
+              document.getElementById("prompt-input").value = interim;
+          } else if (final) {
+              document.getElementById("prompt-input").value = final;
+              searchWithPromptGiven(final)
+          }
+      };
+
+      recognition.onerror = (event) => {
+          console.error("Speech recognition error:", event.error);
+      };
+
+      recognition.onend = () => {
+          // // Automatically restart if listening mode is still active
+          // if (isListening) recognition.start();
+      };
+      }
+
+      recognition.start();
+      isListening = true;
+  }
+
+  function stopListening() {
+      if (recognition) {
+          recognition.stop();
+      }
+      isListening = false;
+
+      document.getElementById("stt-button").innerHTML = "Click to Speak";
+      document.getElementById("prompt-input").placeholder = "\"Click to Speak\" or type something...";
+  }
+
+  async function searchWithPromptGiven(prompt) {
+      stopListening();
+      // document.getElementById("response-area").innerHTML = prompt;
+      // chrome.tts.speak(prompt);
+      await promptModel();
+
+      // TODO if settings is set, continuous listening. listen for prompt word. Restart prompt here
+  }
+
+  // TODO auto resize textarea based on the amount of words spoken
+
+  /* End of TTS STT */
 })();
+
